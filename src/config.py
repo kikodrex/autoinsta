2a01ca035782361dfbae3ea4600da9a07b4d983c
kikodrex <50791270+kikodrex@users.noreply.github.com>
2024-04-19 10:48:24 -0600

    Update config.py

--- src/config.py
+++ src/config.py
@@ -17,7 +17,7 @@ DOWNLOAD_DIR = CURRENT_DIR + '..'+os.sep+'downloads' + os.sep  # Path of folder 
 IS_REMOVE_FILES = 1
 
 # Remove Posted Files Interval
-REMOVE_FILE_AFTER_MINS = 120 #every two hours
+REMOVE_FILE_AFTER_MINS = 30 #every two hours
 
 #--------------------------------------------------------------------------------------------------#
 # Instagram Configurations                                                                         #
@@ -36,22 +36,19 @@ IS_POST_TO_STORY = 1
 FETCH_LIMIT = 10
 
 # Posting interval in Minutes
-POSTING_INTERVAL_IN_MIN = 15  # Every 15 Minutes
+POSTING_INTERVAL_IN_MIN = 5  # Every 15 Minutes
 
 # Scraper interval in Minutes
 SCRAPER_INTERVAL_IN_MIN = 720  # Every 12 hours
 
 # Instagram Username & Password
-USERNAME = "your_username"
-PASSWORD = "your_password"
+USERNAME = "kikodrex69"
+PASSWORD = "Solenn123"
 
 # Account List for scraping
 ACCOUNTS = [
-    "totalgaming_official",
-    "carryminati",
-    "techno_gamerz",
-    "payalgamingg",
-    "dynamo__gaming"
+    "megamoviess"
+    
 ]
 
 # like_and_view_counts_disabled
@@ -61,18 +58,18 @@ LIKE_AND_VIEW_COUNTS_DISABLED = 0
 DISABLE_COMMENTS = 0
 
 # HASHTAGS to add while Posting
-HASHTAGS = "#reels #shorts #likes #follow #Reels-AutoPilot"
+HASHTAGS = "Movies on my Bio #reels #shorts #likes #follow #Reels"
 
 #--------------------------------------------------------------------------------------------------#
 # Youtube Configurations                                                                           #
 #--------------------------------------------------------------------------------------------------#
 
 # IS RUN YOUTUBE SCRAPER
-IS_ENABLED_YOUTUBE_SCRAPING = 1
+IS_ENABLED_YOUTUBE_SCRAPING = 0
 
 
 # IS RUN YOUTUBE SCRAPER
-YOUTUBE_SCRAPING_INTERVAL_IN_MINS = 120
+YOUTUBE_SCRAPING_INTERVAL_IN_MINS = 0
 
 
 # YOUTUBE API KEY
